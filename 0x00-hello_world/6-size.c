b6fb86aba03c49bdb4b45a00e5e466909932a06f
likitak1717 <abufaizsadiq@gmail.com>
2022-10-25 09:23:52 -0700

    remove first file

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{ 
+	printf("size of char: %lu Byte(s)\n", sizeof(char));
+	printf("size of an int: %lu Byte(s)\n", sizeof(int));
+        printf("size of a long int: %lu Byte(s)\n", sizeof(long int));
+        printf("size of a long long int: %lu Byte(s)\n", sizeof(long long int));
+        printf("size of a float: %lu Byte(s)\n", sizeof(float));
+	return (0);
+}	
+
+~                                                        	
+
+~                                                         	
+
+~                                                     	
+                                                        
